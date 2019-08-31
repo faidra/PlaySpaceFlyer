cbd931dded4eb858dc8536ecc545bfc8720d031b
faidra <faidra.windmark@gmail.com>
2019-08-31 18:29:58 +0900

    移動実装

--- Assets/PlaySpaceFlyer/MainController.cs
+++ Assets/PlaySpaceFlyer/MainController.cs
@@ -1,16 +1,41 @@
 ﻿using UnityEngine;
+using System.Linq;
+using UniRx;
 
 public class MainController : MonoBehaviour
 {
     [SerializeField]
-    float PhaseMultiplier;
+    DoubleDrag[] Draggs;
+    [SerializeField]
+    Controller Left;
+    [SerializeField]
+    Controller Right;
+
     [SerializeField]
     float SpeedMultiplier;
     [SerializeField]
     InputSimulator InputSimulator;
 
+    Vector3 currentOffset;
+
+    void Start()
+    {
+        Draggs.Select(d => d.MoveAsObservable()).Merge().Subscribe(AddOffset).AddTo(this);
+
+        Observable.CombineLatest(Left.GripPressed, Right.GripPressed, (l, r) => l && r)
+            .Where(on => on)
+            .Subscribe(_ => currentOffset = Vector3.zero)
+            .AddTo(this);
+    }
+
     void Update()
     {
-        //InputSimulator.SetAllDeviceWorldPosOffset(Vector3.up * Mathf.Sin(Time.time * PhaseMultiplier) * SpeedMultiplier);
+        var offset = Left.PadPressed.Value ? Vector3.zero : currentOffset;
+        InputSimulator.SetAllDeviceWorldPosOffset(offset);
+    }
+
+    void AddOffset(Vector3 grab)
+    {
+        currentOffset += Vector3.up * grab.y * SpeedMultiplier * Time.deltaTime;
     }
 }
