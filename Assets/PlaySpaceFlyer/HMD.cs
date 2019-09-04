797e79494091ab856c07e0c2d631b6f8f82b1efa
faidra <faidra.windmark@gmail.com>
2019-09-05 01:55:03 +0900

    WIP 検証

---
+++ Assets/PlaySpaceFlyer/HMD.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Valve.VR;
+using System.Runtime.InteropServices;
+
+public class HMD : MonoBehaviour
+{
+    public Vector3 Position { get; private set; }
+    public Quaternion Rotation { get; private set; }
+
+    void Update()
+    {
+        var openvr = OpenVR.System;
+        var pose = default(TrackedDevicePose_t);
+        var state = default(VRControllerState_t);
+        openvr.GetControllerStateWithPose(ETrackingUniverseOrigin.TrackingUniverseRawAndUncalibrated, OpenVR.k_unTrackedDeviceIndex_Hmd, ref state, (uint)Marshal.SizeOf<VRControllerState_t>(), ref pose);
+
+        var transform = new SteamVR_Utils.RigidTransform(pose.mDeviceToAbsoluteTracking);
+        Position = transform.pos;
+        Rotation = transform.rot;
+    }
+}
--- Assets/PlaySpaceFlyer/MainController.cs
+++ Assets/PlaySpaceFlyer/MainController.cs
@@ -11,16 +11,26 @@ public class MainController : MonoBehaviour
     [SerializeField]
     Transform Target;
 
+    [SerializeField]
+    DoubleGrip P;
+    [SerializeField]
+    HMD HMD;
+
     bool isMoving;
 
     void Start()
     {
         Moving.IsMovingAsObservable().Subscribe(m => isMoving = m).AddTo(this);
+        P.IsDoubleGrabbingAsObservable()
+            .Where(on => on)
+            .Subscribe(on => InputEmulator.SetAllDeviceWorldRotOffset(Quaternion.Euler(0, Random.Range(0, 360), 0), HMD.Position))
+            .AddTo(this);
     }
 
     void Update()
     {
         var offset = isMoving ? Vector3.zero : Target.transform.position;
-        InputEmulator.SetAllDeviceWorldPosOffset(offset);
+        //InputEmulator.SetAllDeviceWorldPosOffset(offset);
+        Debug.LogError("HMD:"+HMD.Position);
     }
 }
