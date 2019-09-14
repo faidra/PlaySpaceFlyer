ddd3c7a265170e5b2d038d509e224b096f51a248
faidra <faidra.windmark@gmail.com>
2019-09-14 20:11:28 +0900

    グリップ中は他の方向にも移動可能に

---
+++ Assets/PlaySpaceFlyer/LinearMoveModule.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class LinearMoveModule : MonoBehaviour
+{
+    [SerializeField]
+    DoubleDrag Drag;
+    [SerializeField]
+    Controller Controller;
+    [SerializeField]
+    ResetEvent ResetEvent;
+
+    [SerializeField]
+    float SpeedMultiplier;
+
+    void Start()
+    {
+        Drag.MoveAsObservable().Subscribe(AddOffset).AddTo(this);
+
+        ResetEvent.OnResetAsObservable().Subscribe(_ => transform.localPosition = Vector3.zero).AddTo(this);
+    }
+
+    void AddOffset(Vector3 grab)
+    {
+        if (!Controller.GripPressed.Value)
+        {
+            grab.x = 0f;
+            grab.z = 0f;
+        }
+        transform.Translate(grab * SpeedMultiplier * Time.deltaTime);
+    }
+}
