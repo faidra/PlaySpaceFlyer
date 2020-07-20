2164f71eb7030f5b57c06df4761e26d39d9e3d84
faidra <faidra.windmark@gmail.com>
2020-07-21 00:54:43 +0900

    エラーになってたところを修正

--- Assets/PlaySpaceFlyer/InputEmulator.cs
+++ Assets/PlaySpaceFlyer/InputEmulator.cs
@@ -49,7 +49,7 @@ public class InputEmulator : MonoBehaviour
     {
         TrackedDevicePose_t pose = default, gamePose = default;
         OpenVR.Compositor.GetLastPoseForTrackedDeviceIndex(deviceId, ref pose, ref gamePose);
-        ReferenceBaseStationPosition = SteamVR_Utils.GetPosition(pose.mDeviceToAbsoluteTracking);
+        ReferenceBaseStationPosition = new SteamVR_Utils.RigidTransform(pose.mDeviceToAbsoluteTracking).pos;
     }
 
 
