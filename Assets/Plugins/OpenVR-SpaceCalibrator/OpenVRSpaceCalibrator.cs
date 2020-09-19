6454c2335e02ef433a3d31bb4ba6dd2d21bbebca
faidra <faidra.windmark@gmail.com>
2020-09-19 16:09:06 +0900

    add OpenVR-SpaceCalibrator

---
+++ Assets/Plugins/OpenVR-SpaceCalibrator/OpenVRSpaceCalibrator.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OpenVRSpaceCalibrator
+{
+    public static class OpenVRSpaceCalibrator
+    {
+        const string dllName = "OpenVR-SpaceCalibratorClientDLL";
+
+        [DllImport(dllName)]
+        public static extern void Connect();
+
+        [DllImport(dllName)]
+        public static extern void ResetAndDisableOffsets(uint deviceId);
+
+        [DllImport(dllName)]
+        public static extern void SetDeviceOffset(uint deviceId, double x, double y, double z);
+    }
+}
