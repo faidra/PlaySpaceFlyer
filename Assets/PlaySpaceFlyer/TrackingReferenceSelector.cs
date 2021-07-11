edfa2f4ed74cf45531988a4c4ea95e66d787301f
faidra <faidra.windmark@gmail.com>
2021-07-12 01:47:45 +0900

    TrackingReferenceの初期値も取れるように修正

--- Assets/PlaySpaceFlyer/TrackingReferenceSelector.cs
+++ Assets/PlaySpaceFlyer/TrackingReferenceSelector.cs
@@ -5,21 +5,22 @@ using Valve.VR;
 using System.Linq;
 using System;
 
-public class TrackingReferenceSelector : MonoBehaviour
+public sealed class TrackingReferenceSelector : MonoBehaviour
 {
     [SerializeField]
     Dropdown Dropdown;
 
     uint[] _trackingReferenceIds;
-    ReplaySubject<uint> _currentSelected = new ReplaySubject<uint>(1);
+    readonly ReplaySubject<uint> _currentSelected = new ReplaySubject<uint>(1);
 
     void Start()
     {
         var ids = new uint[OpenVR.k_unMaxTrackedDeviceCount];
         var count = OpenVR.System.GetSortedTrackedDeviceIndicesOfClass(ETrackedDeviceClass.TrackingReference, ids, 0u);
-        _trackingReferenceIds = ids.Take((int)count).ToArray();
+        _trackingReferenceIds = ids.Take((int) count).ToArray();
         Dropdown.options = _trackingReferenceIds.Select(id => new Dropdown.OptionData(id.ToString())).ToList();
-        Dropdown.onValueChanged.AsObservable()
+        Dropdown.OnValueChangedAsObservable()
+            .Where(i => i < _trackingReferenceIds.Length)
             .Select(i => _trackingReferenceIds[i])
             .Subscribe(_currentSelected)
             .AddTo(this);
