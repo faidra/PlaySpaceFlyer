e4ed301a69c349f8abc4a47c05a2af7dce896d61
faidra <faidra.windmark@gmail.com>
2021-07-16 01:43:18 +0900

    何も確認してないけどSpaceKick

---
+++ Assets/PlaySpaceFlyer/SpaceKickModule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+public sealed class SpaceKickModule : MonoBehaviour
+{
+    [SerializeField] CenterOfMass centerOfMass;
+    [SerializeField] Tracker footTracker;
+    [SerializeField] float threshold;
+    [SerializeField] float magnify;
+
+    bool hasPrevDiff;
+    Vector3 diff;
+
+    readonly Subject<Vector3> kick = new Subject<Vector3>();
+    public IObservable<Vector3> KickAsObservable() => kick;
+
+    void Update()
+    {
+        if (!centerOfMass.TryGetCOM(out var center))
+        {
+            hasPrevDiff = false;
+            return;
+        }
+
+        var prevDiff = diff;
+        diff = center - footTracker.Position;
+        if (!hasPrevDiff)
+        {
+            hasPrevDiff = true;
+            return;
+        }
+
+        var deltaTime = Time.deltaTime;
+        var velocity = (diff - prevDiff) / deltaTime;
+        var magnitude = velocity.magnitude;
+        if (magnitude < threshold || Vector3.Dot(diff, velocity) < 0)
+        {
+            return;
+        }
+
+        var magnifiedSpeed = threshold + magnify * (magnitude - threshold);
+        kick.OnNext(velocity * (magnifiedSpeed / magnitude));
+    }
+}
---
+++ Assets/PlaySpaceFlyer/SpaceMoveModule.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UniRx;
+
+public sealed class SpaceMoveModule : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] SpaceKickModule[] kickModules;
+    [SerializeField] Vector3 gravity;
+    [SerializeField] Vector3 antiGravity;
+    [SerializeField] float dampingValue;
+    [SerializeField] float dampingRate;
+    
+    Vector3 velocity;
+
+    void Start()
+    {
+        foreach (var kick in kickModules)
+        {
+            kick.KickAsObservable().Subscribe(k => velocity += k).AddTo(this);
+        }
+    }
+    
+    void Update()
+    {
+        var deltaTime = Time.deltaTime;
+        var isUnderWorld = target.localPosition.y < 0;
+        velocity += deltaTime * (isUnderWorld ? antiGravity : gravity);
+        if (isUnderWorld)
+        {
+            velocity *= 1f - Mathf.Min(dampingRate * deltaTime, 1f);
+        }
+        velocity *= 1f - Mathf.Min(dampingValue / velocity.magnitude * deltaTime, 1f);
+
+        target.localPosition += velocity * deltaTime;
+    }
+}
