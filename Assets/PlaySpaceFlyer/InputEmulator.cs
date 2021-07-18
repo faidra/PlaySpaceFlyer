79239c8ce8679501a667571040e22ea53168f7d1
faidra <faidra.windmark@gmail.com>
2021-07-19 02:53:40 +0900

    OpenVRExtensios

--- Assets/PlaySpaceFlyer/InputEmulator.cs
+++ Assets/PlaySpaceFlyer/InputEmulator.cs
@@ -52,8 +52,8 @@ public class InputEmulator : MonoBehaviour
 
     void SetDeviceTransform(uint openVRDeviceId, Vector3 pos, Quaternion rot)
     {
-        var rpos = ToRHand(pos);
-        var rrot = ToRHand(rot);
+        var rpos = pos.ToRHand();
+        var rrot = rot.ToRHand();
         OpenVRSpaceCalibrator.OpenVRSpaceCalibrator.SetDeviceTransform(openVRDeviceId, rpos.x, rpos.y, rpos.z, rrot.x, rrot.y, rrot.z, rrot.w);
     }
 
@@ -66,15 +66,5 @@ public class InputEmulator : MonoBehaviour
     {
         if (OpenVR.System == null) return;
         DisableAllDeviceTransform();
-    }
-
-    static (double x, double y, double z) ToRHand(Vector3 vec)
-    {
-        return (vec.x, vec.y, -vec.z);
-    }
-
-    static (double x, double y, double z, double w) ToRHand(Quaternion rot)
-    {
-        return (-rot.x, -rot.y, rot.z, rot.w);
     }
 }
---
+++ Assets/PlaySpaceFlyer/OpenVRExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Valve.VR;
+using UniRx;
+
+public static class OpenVRExtensions
+{
+    public static (double x, double y, double z) ToRHand(this Vector3 vec)
+    {
+        return (vec.x, vec.y, -vec.z);
+    }
+
+    public static (double x, double y, double z, double w) ToRHand(this Quaternion rot)
+    {
+        return (-rot.x, -rot.y, rot.z, rot.w);
+    }
+
+    public static Vector3 FromRHandPosition(double x, double y, double z)
+    {
+        return new Vector3((float) x, (float) y, -(float) z);
+    }
+
+    public static Quaternion FromRHandRotation(double x, double y, double z, double w)
+    {
+        return new Quaternion(-(float) x, -(float) y, (float) z, (float) w);
+    }
+}
