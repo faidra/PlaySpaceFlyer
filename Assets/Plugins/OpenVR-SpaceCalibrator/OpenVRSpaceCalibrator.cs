ba7ba1d81578b7a6e358fc01306609cbb8ffdbb7
faidra <faidra.windmark@gmail.com>
2021-08-06 01:26:39 +0900

    add OpenVRSpaceCalib.GetDevicePoses

--- Assets/Plugins/OpenVR-SpaceCalibrator/OpenVRSpaceCalibrator.cs
+++ Assets/Plugins/OpenVR-SpaceCalibrator/OpenVRSpaceCalibrator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using Valve.VR;
 
 namespace OpenVRSpaceCalibrator
 {
@@ -14,5 +15,34 @@ namespace OpenVRSpaceCalibrator
 
         [DllImport(dllName)]
         public static extern void SetDeviceTransform(uint deviceId, double x, double y, double z, double qx, double qy, double qz, double qw);
+
+        [DllImport(dllName)]
+        public static extern DevicePoses GetDevicePoses();
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct DevicePoses
+        {
+            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+            public readonly struct DevicePose
+            {
+                public readonly uint openVRID;
+
+                public readonly HmdQuaternion_t qWorldFromDriverRotation;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+                public readonly double[] vecWorldFromDriverTranslation;
+
+                public readonly HmdQuaternion_t qDriverFromHeadRotation;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+                public readonly double[] vecDriverFromHeadTranslation;
+
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+                public readonly double[] vecPosition;
+                public readonly HmdQuaternion_t qRotation;
+            }
+
+            public readonly uint length;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public readonly DevicePose[] devicePoses;
+        }
     }
 }
