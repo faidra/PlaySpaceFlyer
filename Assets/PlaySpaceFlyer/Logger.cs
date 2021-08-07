d48f57a1e7272b687450bb2dea3f78527154faf7
faidra <faidra.windmark@gmail.com>
2021-08-07 22:16:46 +0900

    add logger

---
+++ Assets/PlaySpaceFlyer/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class Logger : MonoBehaviour
+{
+    static Logger instance;
+
+    public static void Log(string str)
+    {
+        if (instance == null) instance = FindObjectOfType<Logger>();
+        if (instance == null)
+        {
+            Debug.LogError("found no logger");
+            return;
+        }
+        instance.LogString(str);
+    }
+
+    [SerializeField] Text text;
+
+    const int Capacity = 1000;
+    readonly StringBuilder builder = new StringBuilder();
+
+    void LogString(string str)
+    {
+        builder.Insert(0, "\n");
+        builder.Insert(0, str);
+        if (builder.Length > Capacity) builder.Length = Capacity;
+        text.text = builder.ToString();
+    }
+}
