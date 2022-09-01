3b176b6ffda107229ad2e20bdc6ae1381797167f
faidra <faidra.windmark@gmail.com>
2022-09-01 20:40:39 +0900

    保存削除

--- Assets/PlaySpaceFlyer/LighthouseOffset.cs
+++ Assets/PlaySpaceFlyer/LighthouseOffset.cs
@@ -1,56 +1,13 @@
 ﻿using UnityEngine;
 
-// 結局保存要らなくなってしまった
 public sealed class LighthouseOffset : MonoBehaviour
 {
     public Vector3 CurrentOffset { get; private set; }
     public Quaternion CurrentRotation { get; private set; } = Quaternion.identity;
 
-    const string QuestOffsetPositionKey = "QuestOffsetPosition.{0}";
-    const string QuestOffsetRotationKey = "QuestOffsetRotation.{0}";
-
-    void Start()
-    {
-        Import();
-    }
-
-    void Import()
-    {
-        if (!PlayerPrefs.HasKey(GetPositionKey(0))) return;
-        Vector3 offset;
-        offset.x = PlayerPrefs.GetFloat(GetPositionKey(0));
-        offset.y = PlayerPrefs.GetFloat(GetPositionKey(1));
-        offset.z = PlayerPrefs.GetFloat(GetPositionKey(2));
-        Quaternion rotation;
-        rotation.x = PlayerPrefs.GetFloat(GetRotationKey(0));
-        rotation.y = PlayerPrefs.GetFloat(GetRotationKey(1));
-        rotation.z = PlayerPrefs.GetFloat(GetRotationKey(2));
-        rotation.w = PlayerPrefs.GetFloat(GetRotationKey(3));
-        CurrentOffset = offset;
-        CurrentRotation = rotation;
-    }
-
-    static string GetPositionKey(int index)
-    {
-        return string.Format(QuestOffsetPositionKey, index);
-    }
-
-    static string GetRotationKey(int index)
-    {
-        return string.Format(QuestOffsetRotationKey, index);
-    }
-
     public void SetValues(Vector3 offset, Quaternion rotation)
     {
         CurrentOffset = offset;
         CurrentRotation = rotation;
-        PlayerPrefs.SetFloat(GetPositionKey(0), offset.x);
-        PlayerPrefs.SetFloat(GetPositionKey(1), offset.y);
-        PlayerPrefs.SetFloat(GetPositionKey(2), offset.z);
-        PlayerPrefs.SetFloat(GetRotationKey(0), rotation.x);
-        PlayerPrefs.SetFloat(GetRotationKey(1), rotation.y);
-        PlayerPrefs.SetFloat(GetRotationKey(2), rotation.z);
-        PlayerPrefs.SetFloat(GetRotationKey(3), rotation.w);
-        PlayerPrefs.Save();
     }
 }
